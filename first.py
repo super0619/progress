81f981c5be61aa0d083ab9cc6891e658fc7326f5
super0619 <2762088343@qq.com>
2020-03-14 22:45:51 +0800

    forcit

--- first.py
+++ first.py
@@ -1,2 +1,43 @@
-print("hello world")
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+
 import numpy as np
+
+test_images_group = np.load("test_images_group.npy")
+test_labels_group = np.load("test_labels_group.npy")
+train_images_group = np.load("train_images_group.npy")
+train_labels_group = np.load("train_labels_group.npy")
+
+train_images, train_labels, test_images, test_labels = test_images_group[0], test_labels_group[0], train_images_group[0], train_labels_group[0]
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (2, 2), activation='relu', input_shape=(12, 12, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (2, 2), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10))
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+history = model.fit(train_images, train_labels, epochs=10, 
+                    validation_data=(test_images, test_labels))
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+print(test_acc)
+
+print(test_acc)
